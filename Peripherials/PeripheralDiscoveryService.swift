e8f895cdd3f5dd9aeb93d02141dd5ad581cacbce
galblank <galblank@gmail.com>
2015-12-18 16:51:36 -0500

    peripherals service

--- Peripherials/PeripheralDiscoveryService.swift
+++ Peripherials/PeripheralDiscoveryService.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class PeripheralDiscoveryService: NSObject {
     
+    var printersList = [AnyObject]()
+    
+    
     override init() {
         super.init()
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "consumeMessage:", name:"internal.searchForPeripherals", object: nil)
@@ -20,6 +23,7 @@ class PeripheralDiscoveryService: NSObject {
         let msg = notif.userInfo!["message"] as! Message
         switch (msg.routingKey){
         case "internal.searchForPeripherals":
+            let peripheralType = msg.params.objectForKey("peripheralType")
             
             break;
         default:
@@ -27,5 +31,25 @@ class PeripheralDiscoveryService: NSObject {
         }
     }
     
+    
+    func connectedPrinters() -> [AnyObject] {
+        printersList = EAAccessoryManager.sharedAccessoryManager().connectedAccessories
+        NSLog("Detected Devices %@", printersList)
+        return printersList
+    }
+    
+    func searchForAllConnectedPrinters() {
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+            self.printersList = SMPort.searchPrinter()
+            NSLog("Found printers %@", self.printersList)
+            var savedPrinters:AnyObject = ["":""]
+            if(NSUserDefaults.standardUserDefaults().objectForKey("printers")?.count > 0){
+                savedPrinters = NSUserDefaults.standardUserDefaults().objectForKey("printers")!
+            }
+            savedPrinters.setObject(self.printersList, forKey: AppConfiguration.sharedConfig().midTidID)
+            NSUserDefaults.standardUserDefaults().setObject(savedPrinters, forKey: "printers")
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
     
 }
--- Peripherials/Printers/PolymorphicPrinterService.swift
+++ Peripherials/Printers/PolymorphicPrinterService.swift
@@ -10,31 +10,9 @@ import UIKit
 
 class PolymorphicPrinterService: NSObject {
 
-    var printersList = [AnyObject]()
-
     func startService(){}
     
     func consumeMessage(notif:NSNotification){}
     
     func printerDidConnect(notif:NSNotification){}
-    
-    func connectedPrinters() -> [AnyObject] {
-        printersList = EAAccessoryManager.sharedAccessoryManager().connectedAccessories
-        NSLog("Detected Devices %@", printersList)
-        return printersList
-    }
-    
-    func searchForPrinters() {
-        dispatch_async(dispatch_get_main_queue()) { () -> Void in
-            self.printersList = SMPort.searchPrinter()
-            NSLog("Found printers %@", self.printersList)
-            var savedPrinters:AnyObject = ["":""]
-            if(NSUserDefaults.standardUserDefaults().objectForKey("printers")?.count > 0){
-                savedPrinters = NSUserDefaults.standardUserDefaults().objectForKey("printers")!
-            }
-            savedPrinters.setObject(self.printersList, forKey: AppConfiguration.sharedConfig().midTidID)
-            NSUserDefaults.standardUserDefaults().setObject(savedPrinters, forKey: "printers")
-            NSUserDefaults.standardUserDefaults().synchronize()
-        }
-    }
 }
